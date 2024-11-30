b1cf72a65f382cf7c83ce2b9241190b6cb3b3cfe
Yang Zhang <yangzhang@live.com>
2024-11-30 13:06:04 +0800

    Make arm build use zst for packaging

--- alarm_docker/entrypoint.sh
+++ alarm_docker/entrypoint.sh
@@ -9,6 +9,8 @@ cat > /etc/sudoers.d/passwd <<__EOF__
 ALL ALL = (ALL) NOPASSWD: ALL
 __EOF__
 
+sed -i 's/pkg.tar.xz/pkg.tar.zst/g' /etc/makepkg.conf
+
 chmod 777 -R .
 for pkg in $1; do
     IFS=, read dir install <<< "${pkg}"
